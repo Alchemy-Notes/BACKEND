12643b4def7563f4915713f3eb2669c25f188fa2
Triana Cerda <trianaRcerda@gmail.com>
2022-01-11 15:59:27 -0800

    added heroku + adjusted password hash

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    password_hash TEXT 
 )
