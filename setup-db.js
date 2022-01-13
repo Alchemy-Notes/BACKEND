a32525fd970dbf46fc1c820e605af1d9ffceaf60
Paul Brubaker <pbrubak@gmail.com>
2022-01-13 14:51:15 -0800

    seed fake data working

--- setup-db.js
+++ setup-db.js
@@ -3,8 +3,6 @@ const setup = require('./data/setup');
 const seedDB = require('./lib/utils/seedDB');
 
 setup(pool)
-  .then(() => {
-    seedDB();
-  })
+  .then(() => seedDB())
   .catch((err) => console.error(err))
   .finally(() => process.exit());
