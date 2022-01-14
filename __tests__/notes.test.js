98e7efd4a63e90703b1e70152203944eedb0b337
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-14 13:30:19 -0800

    switched test syntax to what was in petenotes.test file and deleted that file

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,69 +1,120 @@
-require('dotenv').config();
-
-const { execSync } = require('child_process');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup.js');
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
-const client = require('../lib/client');
+const UserService = require('../lib/services/UserService');
 
-describe('app routes', () => {
-  describe('routes', () => {
-    beforeAll(async () => {
-      execSync('npm run setup-db');
+describe('Notes Routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-      await client.connect();
+  test('notes/new route should create a new note', async () => {
+    const user = await UserService.create({
+      username: 'notAFakeUser',
+      password: 'notAFakePassword',
     });
-    afterAll((done) => client.end(done));
+    const response = await fakeRequest(app)
+      .post('/api/notes/new')
+      .expect('Content-Type', /json/)
+      .send({
+        body: 'Look at my super duper cool JavaScript note. For loops are the best!',
+        title: 'My first note',
+        tags: ['JavaScript'],
+        favorite: false,
+        dateModified: new Date(Date.now()),
+        userId: user.id,
+      });
 
-    test.skip('notes/new route should create a new note', async () => {
-      const response = await fakeRequest(app)
-        .post('/api/notes/new')
-        .expect('Content-Type', /json/)
-        .send({
-          body: 'Look at my super duper cool JavaScript note. For loops are the best!',
-          title: 'My first note',
-          tags: ['JavaScript'],
-          favorite: false,
-          dateModified: new Date(Date.now()),
-          userId: 1,
-        });
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        body: 'Look at my super duper cool JavaScript note. For loops are the best!',
+        title: 'My first note',
+        tags: expect.arrayContaining(['JavaScript']),
+        userId: expect.any(String),
+        favorite: false,
+        dateModified: expect.any(String),
+      })
+    );
+  });
 
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id: expect.any(String),
-          body: 'Look at my super duper cool JavaScript note. For loops are the best!',
-          title: 'My first note',
-          tags: expect.arrayContaining(['JavaScript']),
-          userId: expect.any(String),
-          favorite: false,
-          dateModified: expect.any(String),
-        })
-      );
+  test('notes/edit/:id should update a note', async () => {
+    const user = await UserService.create({
+      username: 'notAFakeUser',
+      password: 'notAFakePassword',
     });
+    const res = await fakeRequest(app)
+      .post('/api/notes/new')
+      .expect('Content-Type', /json/)
+      .send({
+        body: 'Look at my super duper cool JavaScript note. For loops are the best!',
+        title: 'My first note',
+        tags: ['JavaScript'],
+        favorite: false,
+        dateModified: new Date(Date.now()),
+        userId: user.id,
+      });
+    const response = await fakeRequest(app)
+      .put('/api/notes/edit/1')
+      .expect('Content-Type', /json/)
+      .send({
+        body: 'look at my super duper cool JavaScript note. For Loops are the best!',
+        title: 'My First Note',
+        tags: ['JavaScript', 'First', 'For Loops'],
+        favorite: false,
+        dateModified: new Date(Date.now()),
+        userId: res.body.id,
+      });
 
-    test.skip('notes/edit/:id should update a note', async () => {
-      const response = await fakeRequest(app)
-        .put('/api/notes/edit/1')
-        .expect('Content-Type', /json/)
-        .send({
-          body: 'look at my super duper cool JavaScript note. For Loops are the best!',
-          title: 'My First Note',
-          tags: ['JavaScript', 'First', 'For Loops'],
-          favorite: false,
-          dateModified: new Date(Date.now()),
-          userId: 1,
-        });
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        body: 'look at my super duper cool JavaScript note. For Loops are the best!',
+        title: 'My First Note',
+        tags: expect.arrayContaining(['JavaScript', 'First', 'For Loops']),
+        userId: expect.any(String),
+        favorite: false,
+        dateModified: expect.any(String),
+      })
+    );
+  });
 
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id: expect.any(String),
-          body: 'look at my super duper cool JavaScript note. For Loops are the best!',
-          title: 'My First Note',
-          tags: expect.arrayContaining(['JavaScript', 'First', 'For Loops']),
-          userId: expect.any(String),
-          favorite: false,
-          dateModified: expect.any(String),
-        })
-      );
+  test('/notes/delete/:id will delete a note', async () => {
+    const user = await UserService.create({
+      username: 'notAFakeUser',
+      password: 'notAFakePassword',
     });
-  }, 10000);
+    const res = await fakeRequest(app)
+      .post('/api/notes/new')
+      .expect('Content-Type', /json/)
+      .send({
+        body: 'Look at my super duper cool JavaScript note. For loops are the best!',
+        title: 'My first note',
+        tags: ['JavaScript'],
+        favorite: false,
+        dateModified: new Date(Date.now()),
+        userId: user.id,
+      });
+
+    const response = await fakeRequest(app).delete(
+      `/api/notes/delete/${res.body.id}`
+    );
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        body: 'Look at my super duper cool JavaScript note. For loops are the best!',
+        title: 'My first note',
+        tags: expect.arrayContaining(['JavaScript']),
+        userId: expect.any(String),
+        favorite: false,
+        dateModified: expect.any(String),
+      })
+    );
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
 });
