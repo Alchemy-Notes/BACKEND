df33438cd605881a3ffef9090fbd762f9bc5d341
Triana Cerda <trianaRcerda@gmail.com>
2022-01-13 16:56:25 -0800

    deleted /verify route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,8 @@ const { contentType } = require('express/lib/response');
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
+const User = require('../lib/models/User');
+const UserService = require('../lib/services/UserService');
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -14,6 +16,8 @@ describe('app routes', () => {
       await client.connect();
     });
     afterAll((done) => client.end(done));
+
+    const fakeUser = { username: 'lunac', password: 'rainmakerpup' };
 
     test('It should signup a user without oauth', async () => {
       const signInData = await fakeRequest(app)
@@ -26,5 +30,19 @@ describe('app routes', () => {
         id: '6',
       });
     });
+
+    test('should allow user to signin with correct credentials', async () => {
+      const response = await fakeRequest(app)
+        .post('/api/auth/signin')
+        .expect('Content-Type', /json/)
+        .send({ username: 'NegaRingo', password: 'starr12345' });
+
+      expect(response.body).toEqual({
+        id: '6',
+        username: 'NegaRingo',
+      });
+    });
+
+    //_______________end of test________________//
   }, 10000);
 });
